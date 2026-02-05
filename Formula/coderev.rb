31bb2ab052720cf06c00bf5d5f50460a93507ce0
been-there-done-that <57761677+been-there-done-that@users.noreply.github.com>
2026-02-05 12:06:16 +0530

    chore: update coderev to v0.0.3

--- Formula/coderev.rb
+++ Formula/coderev.rb
@@ -1,9 +1,9 @@
 class Coderev < Formula
   desc "A powerful AI-driven grep tool"
   homepage "https://github.com/been-there-done-that/coderev"
-  url "https://github.com/been-there-done-that/coderev/releases/download/v0.0.2/coderev-x86_64-apple-darwin.tar.gz"
-  sha256 "28e834e463af137c46131836c37f0b3360f1832b59943ee5c2eb00ef953604c5"
-  version "0.0.2"
+  url "https://github.com/been-there-done-that/coderev/releases/download/v0.0.3/coderev-x86_64-apple-darwin.tar.gz"
+  sha256 "f9e4bd293100655fed09edbfbdfd989d73cbaba8135a62319499f45942570d88"
+  version "0.0.3"
 
   def install
     bin.install "coderev"
